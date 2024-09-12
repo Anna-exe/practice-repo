58646ff19c03df872fce5d1220514b6216802ce3
Anna Teilane <kastekastee@gmail.com>
2024-09-12 00:07:42 +0000

    remove audio and add testing js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-// Onload functions
+//* Onload functions
 window.onload = () => {
     musicControls();
+    loadCards();
 }
 
 //* Music controls
+
 function musicControls() {
     const backgroundMusic = document.getElementById('background-music');
     const playButton = document.getElementById('play-btn');
@@ -23,4 +25,34 @@ function musicControls() {
     
 }
 
-// Card game
+const frontGrid = document.getElementById('front-grid');
+const cardsFront = [
+    {
+        src: 'assets/images/blonde-angel.webp',
+        alt: 'Blonde Angel'
+    },
+    {
+        src: 'assets/images/dark-angel.webp',
+        alt: 'Dark Angel'
+    },
+    {
+        src: 'assets/images/druid.webp',
+        alt: 'Druid'
+    }
+
+];
+
+
+frontGrid.innerHTML = loader;
+
+
+//* Load cards grid
+function loadCards(){
+    let loader= "";
+    let count = 0;
+    for (let i=0; i < cardsFront.lenght; i++){
+        let s = `<div class='card front'><img src='${card.location}' alt='${card.alt}' id='card${count}'></div>`;
+        loader += s;
+        count++;
+    }
+}
