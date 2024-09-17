d5c1731a872a202663ae764d49572fc73c3f261a
Anna Teilane <kastekastee@gmail.com>
2024-09-17 13:28:17 +0000

    js: display cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,79 +25,37 @@ function musicControls() {
     
 }
 
-const images = [
-    {
-        name: 'blonde angel',
-        image: 'assets/images/blonde-angel.webp'
-    },
-    {
-        name: 'dark angel',
-        image: 'assets/images/dark-angel.webp'
-    },
-    {
-        name: 'druid',
-        image: 'assets/images/druid.webp'
-    },
-    {
-        name: 'ghost',
-        image: 'assets/images/ghost.webp'
-    },
-    {
-        name: 'goddess',
-        image: 'assets/images/goddess.webp'
-    },
-    {
-        name: 'sauron',
-        image: 'assets/images/sauron.webp'
-    },
-    {
-        name: 'silent angel',
-        image: 'assets/images/silent-angel.webp'
-    },
-    {
-        name: 'warrior',
-        image: 'assets/images/warrior.webp'
-    },
-    {
-        name: 'blonde angel',
-        image: 'assets/images/blonde-angel.webp'
-    },
-    {
-        name: 'dark angel',
-        image: 'assets/images/dark-angel.webp'
-    },
-    {
-        name: 'druid',
-        image: 'assets/images/druid.webp'
-    },
-    {
-        name: 'ghost',
-        image: 'assets/images/ghost.webp'
-    },
-    {
-        name: 'goddess',
-        image: 'assets/images/goddess.webp'
-    },
-    {
-        name: 'sauron',
-        image: 'assets/images/sauron.webp'
-    },
-    {
-        name: 'silent angel',
-        image: 'assets/images/silent-angel.webp'
-    },
-    {
-        name: 'warrior',
-        image: 'assets/images/warrior.webp'
-    }
+const cards = [
+        'https://i.ibb.co/YRRFR6v/warrior.webp',
+        'https://i.ibb.co/QkWbTqJ/silent-angel.webp',
+        'https://i.ibb.co/0YX9b8d/sauron.webp',
+        'https://i.ibb.co/V01BQWN/goddess.webp',
+        'https://i.ibb.co/1z6nmgJ/ghost.webp',
+        'https://i.ibb.co/cQR9PfW/druid.webp',
+        'https://i.ibb.co/QnvC210/blonde-angel.webp',
+        'https://i.ibb.co/NjcBtcx/dark-angel.webp',
+        'https://i.ibb.co/YRRFR6v/warrior.webp',
+        'https://i.ibb.co/QkWbTqJ/silent-angel.webp',
+        'https://i.ibb.co/0YX9b8d/sauron.webp',
+        'https://i.ibb.co/V01BQWN/goddess.webp',
+        'https://i.ibb.co/1z6nmgJ/ghost.webp',
+        'https://i.ibb.co/cQR9PfW/druid.webp',
+        'https://i.ibb.co/QnvC210/blonde-angel.webp',
+        'https://i.ibb.co/NjcBtcx/dark-angel.webp'
 ];
+const container = document.getElementById('container');
 
 function loadCards(){
-    
-    const container = document.getElementById('container');
-    images.forEach(image => {
-        const img = document.createElement('img');
-        img.src = image;
-        container.appendChild(img);
-    })
+    let frontGrid = document.getElementById('front-grid');
+    cards.forEach(cards => {
+        let card = document.createElement('img');
+        card.src = cards;
+        frontGrid.append(card)
+    });
+
+    let box = document.getElementById('back-grid');
+    for(let x = 0; x < 3; x++) {
+	let cln = box.cloneNode(true);
+	container.appendChild(cln)
+}
 }
