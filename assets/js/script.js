5b72402d1a5c2bf336f3126d4826337967edc49a
Anna Teilane <kastekastee@gmail.com>
2024-09-18 14:25:06 +0000

    js: wrap each front of cards in divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,39 +27,31 @@ function musicControls() {
 }
 
 var game = document.getElementById('game');
-
+    const frontGrid = document.getElementById('front-grid');
 /**
  * Load cards
  */
 function loadCards(){
     /* Cards front array*/
     let cards = [
-        'https://i.ibb.co/YRRFR6v/warrior.webp',
-        'https://i.ibb.co/QkWbTqJ/silent-angel.webp',
-        'https://i.ibb.co/0YX9b8d/sauron.webp',
-        'https://i.ibb.co/V01BQWN/goddess.webp',
-        'https://i.ibb.co/1z6nmgJ/ghost.webp',
-        'https://i.ibb.co/cQR9PfW/druid.webp',
-        'https://i.ibb.co/QnvC210/blonde-angel.webp',
-        'https://i.ibb.co/NjcBtcx/dark-angel.webp'
+        {src: 'https://i.ibb.co/YRRFR6v/warrior.webp', alt: 'warrior', class: 'card front'},
+        {src: 'https://i.ibb.co/QkWbTqJ/silent-angel.webp', alt: 'silent angel', class: 'card front'},
+        {src: 'https://i.ibb.co/0YX9b8d/sauron.webp', alt: 'sauron', class: 'card front'},
+        {src: 'https://i.ibb.co/V01BQWN/goddess.webp', alt: 'goddess', class: 'card front'},
+        {src: 'https://i.ibb.co/1z6nmgJ/ghost.webp', alt: 'ghost', class: 'card front'},
+        {src: 'https://i.ibb.co/cQR9PfW/druid.webp', alt: 'druid', class: 'card front'},
+        {src: 'https://i.ibb.co/QnvC210/blonde-angel.webp', alt: 'blonde angel', class: 'card front'},
+        {src: 'https://i.ibb.co/NjcBtcx/dark-angel.webp', alt: 'dark angel', class: 'card front'}
     ];
-    /* Display cards front */
-    const frontGrid = document.getElementById('front-grid');
-    cards.forEach(cards => {
-        const frontCard = document.createElement('img');
-        frontCard.src = cards;
-        frontCard.alt = 'front';
-        frontCard.classList = 'card front';
 
-        /* Duplicate cards */
-        frontGrid.append(frontCard)
-        let nodeFront = frontCard;
-        let cloneFront = nodeFront.cloneNode(true);
-        frontGrid.appendChild(cloneFront);
+    /** Display cards front */
+    cards.forEach(cards => {
+        let frontCard = `<div class='card front'><img src='${cards.src}' alt='${cards.alt}' class='${cards.class}'/></div>`;
+        frontGrid.innerHTML += frontCard;
     });
 
     /* Display cards back */
-    let backGrid = document.getElementById("back-grid");
+    let backGrid = document.getElementById('back-grid');
     let blank = document.createElement('img');
     blank.src = 'https://i.ibb.co/4K272ww/blank.webp';
     blank.alt = 'blank';
