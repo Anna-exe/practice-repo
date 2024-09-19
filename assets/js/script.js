9087d44fb175efafb9d7f88b92c8b8c5fafb9ba4
Anna Teilane <kastekastee@gmail.com>
2024-09-19 21:45:02 +0000

    just leave one working function to display cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,27 +51,8 @@ function loadCards(){
         {src: 'https://i.ibb.co/QnvC210/blonde-angel.webp', alt: 'blonde angel'},
         {src: 'https://i.ibb.co/NjcBtcx/dark-angel.webp', alt: 'dark angel'}
     ];
-
-/*
-        let backGrid = document.getElementById('back-grid');
-        let cardBack = document.createElement('div');
-        cardBack.classList = 'card back';
-        backGrid.append(cardBack);
-        
-        let blank = document.createElement('img');
-        blank.src = 'https://i.ibb.co/4K272ww/blank.webp';
-        blank.alt = 'blank';
-        cardBack.append(blank);*/
-
-    /** Display cards front */
     cards.forEach(cards => {
         let card = `<div class='card'><img src='${cards.src}' alt='${cards.alt}'/></div>`;
         game.innerHTML += card;
     });
-    var shuffleCards = cards.sort(() => (Math.random() > .5) ? 2 : -1);
-    for (var i = 0; i < cards.length; i++){
-        let card = document.createElement('div');
-        card.className = 'card';
-        card.innerHTML = shuffleCards[i]
-    }
 }
